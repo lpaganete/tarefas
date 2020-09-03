bd891d2ffd776b41e36bac3bcdc42f9e5a2144de
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-09-03 00:37:36 -0300

    Criando botão e estilizando

--- App.js
+++ App.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { StyleSheet, Text, View, TextInput } from 'react-native';
+import { StyleSheet, Text, View, TextInput, TouchableOpacity } from 'react-native';
 
 export default function App() {
   const [peso, setPeso] = useState(''); // quero que meu cimput comece com uma string vazia 
@@ -23,6 +23,9 @@ export default function App() {
         placeholder="Altura (cm)"
         keyboardType="numeric" //define o tipo de reclado
       />
+      <TouchableOpacity style={styles.button}>
+        <Text style={styles.buttonText}>Calcular</Text>
+      </TouchableOpacity>
     </View>
   );
 }
@@ -46,5 +49,19 @@ const styles = StyleSheet.create({
     padding: 15,
     color: '#fff',
     fontSize: 23,
-  }
+  },
+
+  button: {
+    justifyContent: 'center',
+    alignItems: 'center',
+    margin: 15,
+    padding: 10,
+    backgroundColor: '#41aef4',
+    borderRadius: 10,
+  },
+
+  buttonText: {
+    color: '#fff',
+    fontSize: 25,
+  },
 });
