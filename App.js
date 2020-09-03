6b6133a2550287a6cbbaa5667198d36aa3cda524
Luís Paulo Aganete Reis <luispaulo.aganete@hotmail.com>
2020-09-03 00:07:25 -0300

    Criando title, input e estilizando

--- App.js
+++ App.js
@@ -1,9 +1,18 @@
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState } from 'react';
+import { StyleSheet, Text, View, TextInput } from 'react-native';
 
 export default function App() {
   return (
     <View style={styles.container}>
+      <Text style={styles.title}>Calcule seu IMC</Text>
+
+      <TextInput
+        style={styles.input}
+        value={}
+        onChangeText={} //serve para que cada vez que digitar uma tecla, ele vai salvar em uma 
+        placeholder="Peso (kg)"
+
+      />
     </View>
   );
 }
@@ -11,5 +20,21 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
+    backgroundColor: '#fff4',
   },
+
+  title: {
+    textAlign: 'center',
+    marginTop: 50,
+    fontSize: 30,
+  },
+
+  input: {
+    backgroundColor: '#121212',
+    borderRadius: 10,
+    margin: 15,
+    padding: 15,
+    color: '#fff',
+    fontSize: 23,
+  }
 });
